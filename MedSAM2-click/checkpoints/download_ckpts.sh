92767acf6236fba9d1f779801c43c4924525c07c
zhihe <45589943+pohzhihe@users.noreply.github.com>
2024-12-23 15:16:51 +0800

    Update download_ckpts.sh
    update url for medsam2_pretrain.pth

--- MedSAM2-click/checkpoints/download_ckpts.sh
+++ MedSAM2-click/checkpoints/download_ckpts.sh
@@ -8,24 +8,12 @@
 
 
 # Define the URLs for the checkpoints
-BASE_URL="https://dl.fbaipublicfiles.com/segment_anything_2/072824/"
-sam2_hiera_t_url="${BASE_URL}sam2_hiera_tiny.pt"
-sam2_hiera_s_url="${BASE_URL}sam2_hiera_small.pt"
-sam2_hiera_b_plus_url="${BASE_URL}sam2_hiera_base_plus.pt"
-sam2_hiera_l_url="${BASE_URL}sam2_hiera_large.pt"
+BASE_URL="https://huggingface.co/jiayuanz3/MedSAM2_pretrain/resolve/main/"
+medsam2_url="${BASE_URL}MedSAM2_pretrain.pth"
 
 
 # Download each of the four checkpoints using wget
-echo "Downloading sam2_hiera_tiny.pt checkpoint..."
-wget $sam2_hiera_t_url || { echo "Failed to download checkpoint from $sam2_hiera_t_url"; exit 1; }
-
-echo "Downloading sam2_hiera_small.pt checkpoint..."
-wget $sam2_hiera_s_url || { echo "Failed to download checkpoint from $sam2_hiera_s_url"; exit 1; }
-
-echo "Downloading sam2_hiera_base_plus.pt checkpoint..."
-wget $sam2_hiera_b_plus_url || { echo "Failed to download checkpoint from $sam2_hiera_b_plus_url"; exit 1; }
-
-echo "Downloading sam2_hiera_large.pt checkpoint..."
-wget $sam2_hiera_l_url || { echo "Failed to download checkpoint from $sam2_hiera_l_url"; exit 1; }
+echo "Downloading medsam2_pretrain.pth checkpoint..."
+wget $medsam2_url || { echo "Failed to download checkpoint from $medsam2_url"; exit 1; }
 
 echo "All checkpoints are downloaded successfully."
