64b67d3c3cc0ceb049022559aef058e9dbfd6a78
zhihe <45589943+pohzhihe@users.noreply.github.com>
2025-04-05 17:58:52 +0800

    Add files via upload

--- MedSAM2/__init__.py
+++ MedSAM2/__init__.py
@@ -1,28 +1,93 @@
-# flake8: noqa
+import gdown
 
-import logging
-import sys
-
-from qtpy import QT_VERSION
+from .efficient_sam import EfficientSam
+from .segment_anything_model import SegmentAnythingModel
 
 
-__appname__ = "labelme"
+class SegmentAnythingModelVitB(SegmentAnythingModel):
+    name = "SegmentAnything (speed)"
 
-# Semantic Versioning 2.0.0: https://semver.org/
-# 1. MAJOR version when you make incompatible API changes;
-# 2. MINOR version when you add functionality in a backwards-compatible manner;
-# 3. PATCH version when you make backwards-compatible bug fixes.
-# e.g., 1.0.0a0, 1.0.0a1, 1.0.0b0, 1.0.0rc0, 1.0.0, 1.0.0.post0
-__version__ = "5.5.0"
+    def __init__(self):
+        super().__init__(
+            encoder_path=gdown.cached_download(
+                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_b_01ec64.quantized.encoder.onnx",  # NOQA
+                md5="80fd8d0ab6c6ae8cb7b3bd5f368a752c",
+            ),
+            decoder_path=gdown.cached_download(
+                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_b_01ec64.quantized.decoder.onnx",  # NOQA
+                md5="4253558be238c15fc265a7a876aaec82",
+            ),
+        )
 
-QT4 = QT_VERSION[0] == "4"
-QT5 = QT_VERSION[0] == "5"
-del QT_VERSION
 
-PY2 = sys.version[0] == "2"
-PY3 = sys.version[0] == "3"
-del sys
+class SegmentAnythingModelVitL(SegmentAnythingModel):
+    name = "SegmentAnything (balanced)"
 
-from labelme.label_file import LabelFile
-from labelme import testing
-from labelme import utils
+    def __init__(self):
+        super().__init__(
+            encoder_path=gdown.cached_download(
+                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_l_0b3195.quantized.encoder.onnx",  # NOQA
+                md5="080004dc9992724d360a49399d1ee24b",
+            ),
+            decoder_path=gdown.cached_download(
+                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_l_0b3195.quantized.decoder.onnx",  # NOQA
+                md5="851b7faac91e8e23940ee1294231d5c7",
+            ),
+        )
+
+
+class SegmentAnythingModelVitH(SegmentAnythingModel):
+    name = "SegmentAnything (accuracy)"
+
+    def __init__(self):
+        super().__init__(
+            encoder_path=gdown.cached_download(
+                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_h_4b8939.quantized.encoder.onnx",  # NOQA
+                md5="958b5710d25b198d765fb6b94798f49e",
+            ),
+            decoder_path=gdown.cached_download(
+                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_h_4b8939.quantized.decoder.onnx",  # NOQA
+                md5="a997a408347aa081b17a3ffff9f42a80",
+            ),
+        )
+
+
+class EfficientSamVitT(EfficientSam):
+    name = "EfficientSam (speed)"
+
+    def __init__(self):
+        super().__init__(
+            encoder_path=gdown.cached_download(
+                url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vitt_encoder.onnx",  # NOQA
+                md5="2d4a1303ff0e19fe4a8b8ede69c2f5c7",
+            ),
+            decoder_path=gdown.cached_download(
+                url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vitt_decoder.onnx",  # NOQA
+                md5="be3575ca4ed9b35821ac30991ab01843",
+            ),
+        )
+
+
+class EfficientSamVitS(EfficientSam):
+    name = "EfficientSam (accuracy)"
+
+    def __init__(self):
+        super().__init__(
+            encoder_path=gdown.cached_download(
+                url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vits_encoder.onnx",  # NOQA
+                md5="7d97d23e8e0847d4475ca7c9f80da96d",
+            ),
+            decoder_path=gdown.cached_download(
+                url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vits_decoder.onnx",  # NOQA
+                md5="d9372f4a7bbb1a01d236b0508300b994",
+            ),
+        )
+
+
+MODELS = [
+    SegmentAnythingModelVitB,
+    SegmentAnythingModelVitL,
+    SegmentAnythingModelVitH,
+    EfficientSamVitT,
+    EfficientSamVitS,
+]
